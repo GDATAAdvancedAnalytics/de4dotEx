8d2eef566cec5df9da8a74c928201ef8869d22ec
Melissa Eckardt <melissa.eckardt@gdata-adan.de>
2026-04-02 14:06:17 +0200

    Bea: Remove unneeded setting of DllDirectory

--- de4dot.code/deobfuscators/ConfuserEx/x86/Bea/Engine.cs
+++ de4dot.code/deobfuscators/ConfuserEx/x86/Bea/Engine.cs
@@ -1,33 +1,10 @@
 using System;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace de4dot.Bea
 {
 	public static class BeaEngine
 	{
-		// 'de4dot\bin\de4dot.blocks.dll' -> 'de4dot\bin\'
-		private static readonly string ExecutingPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-
-		static BeaEngine()
-		{
-			//TODO: Better handle native DLL discovery
-			if (IsWindows())
-				SetDllDirectory(ExecutingPath);
-		}
-
-		private static bool IsWindows()
-		{
-#if NET5_0_OR_GREATER
-			return OperatingSystem.IsWindows();
-#else
-			return Environment.OSVersion.Platform == PlatformID.Win32NT;
-#endif
-		}
-
-		[DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
-		private static extern bool SetDllDirectory(string lpPathName);
-
 		[DllImport("BeaEngine")]
 		public static extern int Disasm([In, Out, MarshalAs(UnmanagedType.LPStruct)] Disasm disasm);
 
